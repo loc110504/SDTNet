7c5a5d210d863f65ae791ccdd1dd32d1653d7f4a
loc110504 <loc11052004@gmail.com>
2025-09-20 18:36:29 +0000

    update

--- code/train_ablation/ablation.sh
+++ code/train_ablation/ablation.sh
@@ -1,3 +1,7 @@
-python TABNet1.py
-python TABNet2.py
-python TABNet_nocutout.py
+# python TABNet1.py
+# python TABNet2.py
+# python TABNet_nocutout.py
+
+python TABNet4.py
+python TABNet5.py
+python TABNet6.py
