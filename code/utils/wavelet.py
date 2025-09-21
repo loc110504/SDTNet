545c66f969d32161fc969ac4883c3e0e0fe6c308
loc110504 <loc11052004@gmail.com>
2025-09-21 18:11:26 +0000

    d

--- code/utils/wavelet.py
+++ code/utils/wavelet.py
@@ -2,28 +2,33 @@ import pywt
 import numpy as np
 
 def decompose_dwt2(image: np.ndarray, wavelet: str = 'db2'):
-    """
-    Thực hiện DWT2D, tách thành LL, LH, HL, HH.
-    """
     LL, (LH, HL, HH) = pywt.dwt2(image, wavelet)
     return LL, LH, HL, HH
 
+def minmax01(x: np.ndarray, eps=1e-8):
+    x = x.astype(np.float32)
+    mn, mx = x.min(), x.max()
+    if abs(mx - mn) < eps:
+        return np.zeros_like(x)
+    return (x - mn) / (mx - mn)
 
-def extract_LH( original,
-                wavelet: str = 'db2'):
+def extract_LH(original: np.ndarray, wavelet: str = 'db2'):
     """
-    Tái tạo hai thành phần:
+    Tái tạo và scale về [0,1]:
       - L: chỉ giữ LL, zero hóa LH/HL/HH
-      - H: chỉ giữ chi tiết LH+HL+HH, zero hóa LL
-    Trả về hai ảnh cùng kích thước với original.
+      - H: chỉ giữ LH+HL+HH, zero hóa LL
     """
-    
-    LL, LH, HL, HH = decompose_dwt2(original)
+    LL, LH, HL, HH = decompose_dwt2(original, wavelet)
     zeros_high = np.zeros_like(LH)
+
     # Low-frequency only
     L = pywt.idwt2((LL, (zeros_high, zeros_high, zeros_high)), wavelet)
 
     # High-frequency only
     H = pywt.idwt2((np.zeros_like(LL), (LH, HL, HH)), wavelet)
+
+    # Scale về [0,1] trước khi trả về
+    L = minmax01(L)
+    H = minmax01(H)
 
     return L, H
